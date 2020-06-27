63fb1327c72690e37d3f97031f6d0a14e4bdcfea
Arif Rabbani <arif.93rabbani@gmail.com>
2020-06-27 23:23:24 +0530

    Fixes

--- src/app/events/events.constant.ts
+++ src/app/events/events.constant.ts
@@ -4,7 +4,7 @@ export const events: IEvent[] = [
     new Event('Mumbai', '2020-06-10', '2020-06-12'),
     new Event('Bangalore', '2020-07-14', '2020-07-19'),
     new Event('Sewree', '2020-07-20', '2020-07-20'),
-    new Event('Delhi', '2020-07-29', '2020-08-01'),
+    new Event('Delhi', '2020-07-29', '2020-07-31'),
     new Event('Pune', '2020-08-15', '2020-08-24'),
     new Event('Kolkata', '2020-08-30', '2020-08-30'),
     new Event('Hyderabad', '2020-09-10', '2020-09-12'),
